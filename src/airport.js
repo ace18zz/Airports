5c664bd551a6fc85fd0950db18685e217cf03b54
zzhao30 <zzhao30@sheffield.ac.uk>
2023-07-07 17:32:19 +0800

    Airport done

--- src/airport.js
+++ src/airport.js
@@ -1,1 +1,33 @@
-
+class Airport {
+    constructor(name, airportCapacity = 10) {
+      this.name = name;
+      this.airportCapacity = airportCapacity;
+      this.currentPlanes = [];
+    }
+  
+    isFull() {
+      return this.currentPlanes.length >= this.airportCapacity;
+    }
+  
+    landPlane(plane) {
+      if (this.isFull()) {
+        throw new Error('Cannot land plane');
+      }
+  
+      this.currentPlanes.push(plane);
+      plane.setStatus('landed');
+      plane.setCurrentLocation(this.name);
+    }
+  
+    takeoffPlane(plane) {
+      if (!this.currentPlanes.includes(plane)) {
+        throw new Error('This plane is not at this airport.');
+      }
+  
+      this.currentPlanes = this.currentPlanes.filter(p => p !== plane);
+      plane.setStatus('in_air');
+      plane.setCurrentLocation(null);
+    }
+  }
+  
+  module.exports = Airport;
